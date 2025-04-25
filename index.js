b635cba5387e8cfd12c8fa299b10ba9d206f3c98
maniac15 <datmmo1511@gmail.com>
2025-04-25 14:40:48 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once('ready', () => {
+  console.log(`Đã đăng nhập với tên ${client.user.tag}`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content.startsWith('!hello')) {
+    message.reply('Xin chào! Tôi là bot của bạn.');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
